d2a5f554e55c93c1a8b7aea2600b1f3080b6098b
Philippe Fanaro <philippefanaro@gmail.com>
2023-04-29 23:01:48 -0300

    first steps towards an options page

---
+++ src/options.ts
@@ -0,0 +1,5 @@
+const settingsForm: HTMLFormElement = document.body.querySelector('form');
+
+settingsForm.onsubmit = () => {
+  const nameInput: HTMLInputElement = document.body.querySelector('input[name="home"]');
+}
