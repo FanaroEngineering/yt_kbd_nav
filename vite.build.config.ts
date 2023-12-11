d699123b4aaabf1e7c1b894f5d65fd2e6f5ee3cc
Philippe Fanaro <philippefanaro@gmail.com>
2023-12-11 08:10:43 -0300

    cleanup and reorganization

--- vite.build.config.ts
+++ vite.build.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vite"
 
 export default defineConfig({
   build: {
@@ -7,4 +7,4 @@ export default defineConfig({
       name: "YT Kbd Nav",
     },
   },
-});
+})
